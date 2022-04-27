4d5ad90a9476a301e01980358c6361043c442442
Tikijavi <74424481+Tikijavi@users.noreply.github.com>
2022-04-27 17:10:33 +0200

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,43 @@
+# Creació d'un grup de seguretat nou per a una subxarxa pública
+resource "aws_security_group" "SG_public_subnet" {
+  name        = "Grup_seguretat_Rocketchat"
+  description = "Permet SSH i HTTP per a Rocketchat"
+  vpc_id      =  aws_vpc.my_vpc.id                   
+
+  ingress {
+    description = "SSH desde VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    description = "HTTP desde VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
