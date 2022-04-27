5d019056501e2c6c92584f4ebe5aafa1ec7984c4
Joel <57760071+JoelCasas11@users.noreply.github.com>
2022-04-27 17:15:47 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -35,3 +35,25 @@ variable "instance_type_ec2" {
   type        = string
   default     = "t2.micro"
 }
+
+#Cloudwatch
+variable "logs_path" {
+  description = "Path of the logs in CloudWatch"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Resource tags"
+}
+
+variable "log_group_retention_in_days" {
+  description = "(Optional) Specifies the number of days you want to retain log events in the specified log group. Default to 30 days"
+  type        = number
+  default     = 30
+}
+
+variable "log_group_kms_key_id" {
+  description = "The ARN of the KMS Key to use when encrypting log data. Please note, after the AWS KMS CMK is disassociated from the log group, AWS CloudWatch Logs stops encrypting newly ingested data for the log group. All previously ingested data remains encrypted, and AWS CloudWatch Logs requires permissions for the CMK whenever the encrypted data is requested."
+  default     = ""
+}
