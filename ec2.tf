1032529de1fa2fd00e3ac7e6112f82e95cb1847b
Joel <57760071+JoelCasas11@users.noreply.github.com>
2022-04-27 17:14:50 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -19,3 +19,16 @@ resource "aws_instance" "Rocket-Chat" {
   command = "echo ${aws_instance.Rocket-Chat.public_ip} > publicIP.txt"
  }
 }
+
+#CloudWatch
+resource "aws_cloudwatch_log_group" "log_group" {
+  name              = var.logs_path
+  retention_in_days = var.log_group_retention_in_days
+  kms_key_id        = var.log_group_kms_key_id
+  tags              = var.tags
+}
+
+resource "aws_cloudwatch_log_stream" "log_stream" {
+  name           = var.logs_path
+  log_group_name = aws_cloudwatch_log_group.log_group.name
+}
